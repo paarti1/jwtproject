6f0a286aca8513b471b1d2c9b3d2e61a8081523c
paarti1 <capcar314@gmail.com>
2025-06-27 09:16:13 +0530

    jwt

---
+++ JWT/src/main/java/com/aarti/springsecdemo/service/UserService.java
@@ -0,0 +1,23 @@
+package com.aarti.springsecdemo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.aarti.springsecdemo.dao.UserRepo;
+import com.aarti.springsecdemo.model.User;
+
+@Service
+public class UserService {
+	
+@Autowired
+	private UserRepo repo;
+private BCryptPasswordEncoder encoder=new BCryptPasswordEncoder(12);
+
+	public User saveUser(User user) {
+		user.setPassword(encoder.encode(user.getPassword()));
+		System.out.println(user.getPassword());
+	return repo.save(user) ;
+		
+	}
+}
